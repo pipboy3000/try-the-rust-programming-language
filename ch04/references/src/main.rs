fd066d393128f6a7f2a4003ed0a32129c4836c81
Asai Masami <shakeforme@gmail.com>
2020-07-30 18:57:29 +0900

    参照と借用まで完了

---
+++ ch04/references/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut s1 = String::from("hello");
+
+    change(&mut s1);
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let _reference_to_nothing = no_dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
+}
