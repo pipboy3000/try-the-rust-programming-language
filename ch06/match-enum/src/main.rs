361e627f185fbb0a0377af0829399f09cb7d77ae
Asai Masami <shakeforme@gmail.com>
2020-08-11 19:06:34 +0900

    enumを完了

---
+++ ch06/match-enum/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let cent = value_in_cents(Coin::Quarter(UsState::Alabama));
+    println!("value is {}", cent);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("Result is {:?}", six);
+    let none = plus_one(None);
+    println!("Result is {:?}", none);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
